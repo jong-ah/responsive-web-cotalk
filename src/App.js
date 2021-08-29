b5f4e89e41496b5519cef02ce2a4b38f75b3921a
jong-ah <jonga6431@gmail.com>
2021-08-30 07:14:06 +0900

    Fix typo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-sm-4 col-md-6 col-lg-7">
-            <div style={{ backgroundColor: 'lightGray' }}>Cloumn1</div>
+            <div style={{ backgroundColor: 'lightGray' }}>Service image</div>
           </div>
           <div className="col-sm-4 col-md-6 col-lg-5">
-            <div style={{ backgroundColor: 'lightGray' }}>Cloumn2</div>
+            <div style={{ backgroundColor: 'lightGray' }}>Service contents</div>
           </div>
         </div>
       </div>
