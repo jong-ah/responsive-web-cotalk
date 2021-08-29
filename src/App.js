f3d8d15313554d33d6b4dba7e79125338dbaf43a
jong-ah <jonga6431@gmail.com>
2021-08-30 05:43:58 +0900

    Add responsive-web-cotalk presentation

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4 col-md-6 col-lg-7">
+            <div style={{ backgroundColor: 'lightGray' }}>Cloumn1</div>
+          </div>
+          <div className="col-sm-4 col-md-6 col-lg-5">
+            <div style={{ backgroundColor: 'lightGray' }}>Cloumn2</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
